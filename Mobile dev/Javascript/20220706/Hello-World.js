74ec4a44c9228e7a9a8ff8ceed4433024d07e8af
unknown <suryaagus071@gmail.com>
2022-07-06 16:04:57 +0700

    Hari ke 2

--- Mobile dev/Javascript/20220706/Hello-World.js
+++ Mobile dev/Javascript/20220706/Hello-World.js
@@ -2,8 +2,5 @@ const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!'; //Heading di HTML karna menuju kesana dengan fungsi querySelector
 
 let myVariable= document.querySelector('p');
- myVariable.textContent = 'ANTOOO';
-
-
-}
+myVariable.textContent = 'ANTOOO';
 
--- Mobile dev/Javascript/20220706/Variable.js
+++ Mobile dev/Javascript/20220706/Variable.js
@@ -6,7 +6,7 @@ let iceCream = 'chocolate';
 if(iceCream === 'chocolate') {
   ('Yay, I love chocolate ice cream!'); // alert untuk peringatan yang akan muncul di minitab browser
 } else {
-  ('Awwww, but chocolate is my favorite...');
+('Awwww, but chocolate is my favorite...');
 }
 console.log(iceCream)
 
